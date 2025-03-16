3fbc412a3762b74f4235c3e2fe5829076dbac885
Aguru Itabashi <itabashi.a@gmail.com>
2025-03-16 12:11:23 +0900

    blogコントローラーのindex,create,showを実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,4 +3,24 @@ class BlogsController < ApplicationController
     blogs = Blog.all
     render json: blogs
   end
+
+  def create
+    blog = Blog.new(blog_params)
+    if blog.save
+      head :created
+    else
+      render json: { errors: blog.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    blog = Blog.find(params[:id])
+    render json: blog
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
 end
